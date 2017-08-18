1f726e64fd9b5841302544dbbc42726c8b4087f0
elpinal <elpinal@users.noreply.github.com>
2017-08-18 21:50:54 +0900

    Enable to deal with variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,33 @@
-#[derive(Debug, PartialEq)]
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Clone)]
 enum Type {
     Int,
     Fun(Box<Type>, Box<Type>),
 }
 
 enum Expr {
+    Var(String),
     Int(isize),
     App(Box<Expr>, Box<Expr>),
 }
 
-fn ti(e: Expr) -> Option<Type> {
-    match e {
-        Expr::Int(_) => Some(Type::Int),
-        Expr::App(_, _) => None,
+struct Env {
+    vars: HashMap<String, Type>,
+}
+
+impl Env {
+    fn ti(&self, e: Expr) -> Option<Type> {
+        match e {
+            Expr::Var(s) => {
+                match self.vars.get(&s) {
+                    Some(t) => Some(t.clone()),
+                    None => None,
+                }
+            },
+            Expr::Int(_) => Some(Type::Int),
+            Expr::App(_, _) => None,
+        }
     }
 }
 
@@ -22,6 +37,10 @@ mod tests {
 
     #[test]
     fn test_ti() {
-        assert_eq!(ti(Expr::Int(12)), Some(Type::Int));
+        let env = Env{vars: HashMap::new()};
+        assert_eq!(env.ti(Expr::Int(12)), Some(Type::Int));
+        let mut env = Env{vars: HashMap::new()};
+        env.vars.insert(String::from("f"), Type::Fun(Box::new(Type::Int), Box::new(Type::Int)));
+        assert_eq!(env.ti(Expr::Var(String::from("f"))), Some(Type::Fun(Box::new(Type::Int), Box::new(Type::Int))));
     }
 }
