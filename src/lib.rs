2aa7c2a9c0e5ccca8082e02e001419a1ddc94764
elpinal <elpinal@users.noreply.github.com>
2017-08-18 21:24:32 +0900

    Add basic types and "ti" function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,27 @@
+#[derive(Debug, PartialEq)]
+enum Type {
+    TInt,
+    TFun(Box<Type>, Box<Type>),
+}
+
+enum Expr {
+    EInt,
+    EApp,
+}
+
+fn ti(e: Expr) -> Option<Type> {
+    match e {
+        Expr::EInt => Some(Type::TInt),
+        Expr::EApp => None,
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn test_ti() {
+        assert_eq!(ti(Expr::EInt), Some(Type::TInt));
     }
 }
