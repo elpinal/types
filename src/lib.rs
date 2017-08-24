4fb12f94dd4373a9b1d874029098a2e4bcb37c33
elpinal <elpinal@users.noreply.github.com>
2017-08-24 22:39:49 +0900

    Remove Env

--- src/lib.rs
+++ src/lib.rs
@@ -226,71 +226,9 @@ impl TI {
     }
 }
 
-struct Env {
-    vars: HashMap<String, Type>,
-}
-
-impl Env {
-    fn new() -> Env {
-        Env { vars: HashMap::new() }
-    }
-
-    fn def(&mut self, name: &str, e: Expr) {
-        let t = self.ti(e).unwrap();
-        self.vars.insert(String::from(name), t);
-    }
-
-    fn ti(&self, e: Expr) -> Option<Type> {
-        match e {
-            Expr::Var(s) => {
-                match self.vars.get(&s) {
-                    Some(t) => Some(t.clone()),
-                    None => None,
-                }
-            }
-            Expr::Int(_) => Some(Type::Int),
-            Expr::App(box f, box x) => {
-                match self.ti(f) {
-                    Some(Type::Fun(box t1, box t2)) => {
-                        match self.ti(x) {
-                            Some(ref t) if t == &t1 => Some(t2),
-                            _ => None,
-                        }
-                    }
-                    _ => None,
-                }
-            }
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_ti() {
-        let env = Env::new();
-        assert_eq!(env.ti(Expr::Int(12)), Some(Type::Int));
-
-        let mut env = Env::new();
-        env.vars.insert(
-            String::from("f"),
-            Type::Fun(Box::new(Type::Int), Box::new(Type::Int)),
-        );
-        assert_eq!(
-            env.ti(Expr::Var(String::from("f"))),
-            Some(Type::Fun(Box::new(Type::Int), Box::new(Type::Int)))
-        );
-
-        assert_eq!(
-            env.ti(Expr::App(
-                Box::new(Expr::Var(String::from("f"))),
-                Box::new(Expr::Int(12)),
-            )),
-            Some(Type::Int)
-        );
-    }
 
     #[test]
     fn test_type_ftv() {
