eac459206b98079de2b32abd8afd6050b29ca4ac
elpinal <elpinal@users.noreply.github.com>
2018-01-15 17:53:23 +0900

    Add Term and some functions

--- src/system_f.rs
+++ src/system_f.rs
@@ -57,6 +57,93 @@ impl Type {
     }
 }
 
+#[derive(Clone, Debug, PartialEq)]
+enum Term {
+    Var(isize, isize),
+    Abs(String, Type, Box<Term>),
+    App(Box<Term>, Box<Term>),
+    TAbs(String, Box<Term>),
+    TApp(Box<Term>, Type),
+    Pack(Type, Box<Term>, Type),
+    Unpack(String, String, Box<Term>, Box<Term>),
+}
+
+impl Term {
+    fn map<F, G>(self, onvar: &F, ontype: &G, c: isize) -> Term
+    where
+        F: Fn(isize, isize, isize) -> Term,
+        G: Fn(isize, Type) -> Type,
+    {
+        match self {
+            Term::Var(x, n) => onvar(c, x, n),
+            Term::Abs(x, ty, t) => {
+                Term::Abs(x, ontype(c, ty), Box::new(t.map(onvar, ontype, c + 1)))
+            }
+            Term::App(t1, t2) => {
+                Term::App(
+                    Box::new(t1.map(onvar, ontype, c)),
+                    Box::new(t2.map(onvar, ontype, c)),
+                )
+            }
+            Term::TAbs(i, t) => Term::TAbs(i, Box::new(t.map(onvar, ontype, c + 1))),
+            Term::TApp(t, ty) => Term::TApp(Box::new(t.map(onvar, ontype, c)), ontype(c, ty)),
+            Term::Pack(ty1, t, ty2) => {
+                Term::Pack(
+                    ontype(c, ty1),
+                    Box::new(t.map(onvar, ontype, c)),
+                    ontype(c, ty2),
+                )
+            }
+            Term::Unpack(tyi, ti, t1, t2) => {
+                Term::Unpack(
+                    tyi,
+                    ti,
+                    Box::new(t1.map(onvar, ontype, c)),
+                    Box::new(t2.map(onvar, ontype, c + 2)),
+                )
+            }
+        }
+    }
+
+    fn shift_above(self, d: isize, c: isize) -> Term {
+        let f = |c, x, n| if x >= c {
+            Term::Var(x + d, n + d)
+        } else {
+            Term::Var(x, n + d)
+        };
+        let g = |c, ty: Type| ty.shift_above(d, c);
+        self.map(&f, &g, c)
+    }
+
+    fn shift(self, d: isize) -> Term {
+        self.shift_above(d, 0)
+    }
+
+    fn subst(self, t: &Term, j: isize) -> Term {
+        let f = |j, x, n| if x == j {
+            t.clone().shift(j)
+        } else {
+            Term::Var(x, n)
+        };
+        let g = |_, t| t;
+        self.map(&f, &g, j)
+    }
+
+    fn subst_top(self, t: Term) -> Term {
+        self.subst(&t.shift(1), 0).shift(-1)
+    }
+
+    fn subst_type(self, ty1: Type, j: isize) -> Term {
+        let f = |_, x, n| Term::Var(x, n);
+        let g = |j, ty2: Type| ty2.subst(&ty1, j);
+        self.map(&f, &g, j)
+    }
+
+    fn subst_type_top(self, ty: Type) -> Term {
+        self.subst_type(ty.shift(1), 0).shift(-1)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
