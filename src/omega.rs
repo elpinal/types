859bd110239188807ecc7257f3908c16679b80a5
elpinal <elpinal@users.noreply.github.com>
2018-01-16 23:19:36 +0900

    Modify type_of

--- src/omega.rs
+++ src/omega.rs
@@ -59,20 +59,19 @@ impl Type {
 impl Term {
     fn type_of(&self, ctx: Context) -> Result<Type, TypeError> {
         use self::Term::*;
-        use self::Type::*;
         use self::TypeError::*;
         match *self {
             Var(x, n) => ctx.get_type(x).ok_or_else(|| Unbound(x, ctx)),
             Abs(ref i, ref ty1, ref t) => {
-                let ctx1 = ctx.clone().add(i.clone(), ty1.clone());
+                let ctx1 = ctx.clone().add(i.clone(), Binding::Term(ty1.clone()));
                 let ty2 = t.type_of(ctx1)?;
-                let x = Arr(Box::new(ty1.clone()), Box::new(ty2));
+                let x = Type::Arr(Box::new(ty1.clone()), Box::new(ty2));
                 Ok(x)
             }
             App(ref t1, ref t2) => {
                 let ty1 = t1.type_of(ctx.clone())?;
                 match ty1 {
-                    Arr(ty11, ty12) => {
+                    Type::Arr(ty11, ty12) => {
                         unimplemented!()
                     }
                     _ => Err(NotArr(ty1)),
