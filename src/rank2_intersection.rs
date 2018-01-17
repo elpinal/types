5973ad4ae47b01bf3b183e987e619f890d6b4607
elpinal <elpinal@users.noreply.github.com>
2018-01-17 21:25:23 +0900

    Add Term

--- src/rank2_intersection.rs
+++ src/rank2_intersection.rs
@@ -18,3 +18,9 @@ type Rank1 = RankN<SimpleType>;
 type Rank2 = RankN<Rank1>;
 
 struct Subst(HashMap<usize, SimpleType>);
+
+enum Term {
+    Var(usize, usize),
+    Abs(Box<Term>),
+    App(Box<Term>, Box<Term>),
+}
