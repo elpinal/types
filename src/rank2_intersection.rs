79da84b17e74365a129e0776724cf888a92eca10
elpinal <elpinal@users.noreply.github.com>
2018-01-17 22:04:22 +0900

    Add Rank1::simple_types

--- src/rank2_intersection.rs
+++ src/rank2_intersection.rs
@@ -1,16 +1,19 @@
 //! A system of rank 2 intersection types.
 
+use std::cmp::Ordering;
+
 use std::collections::HashMap;
 use std::collections::HashSet;
 
+#[derive(Eq, Hash, PartialEq)]
 enum SimpleType {
     Var(usize, usize),
     Arr(Box<SimpleType>, Box<SimpleType>),
 }
 
+#[derive(PartialEq)]
 enum Rank1 {
-    Var(usize, usize),
-    Arr(SimpleType, SimpleType),
+    Simple(SimpleType),
     Intersection(Box<Rank1>, Box<Rank1>),
 }
 
@@ -32,4 +35,21 @@ struct Env<T>(Vec<T>);
 
 trait Types {
     fn ftv(&Self) -> HashSet<usize>;
+}
+
+impl Rank1 {
+    fn simple_types(self) -> HashSet<SimpleType> {
+        use self::Rank1::*;
+        let mut types = HashSet::new();
+        match self {
+            Simple(t) => {
+                types.insert(t);
+            }
+            Intersection(t1, t2) => {
+                types.extend(t1.simple_types());
+                types.extend(t2.simple_types());
+            }
+        }
+        types
+    }
 }
