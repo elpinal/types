caffbdc85609183312e5d0bad5d242522dc2644b
elpinal <elpinal@users.noreply.github.com>
2018-01-18 23:41:27 +0900

    Add rank2_intersection2 module

---
+++ src/rank2_intersection2.rs
@@ -0,0 +1,22 @@
+//! Another approach to rank 2 intersection types.
+
+enum SimpleType {
+    Var(String),
+    Arr(Box<SimpleType>, Box<SimpleType>),
+}
+
+enum Rank1 {
+    Simple(SimpleType),
+    Intersection(Box<Rank1>, Box<Rank1>),
+}
+
+enum Rank2 {
+    Simple(SimpleType),
+    Arr(Rank1, Box<Rank2>),
+}
+
+enum Term {
+    Var(usize, usize),
+    Abs(Box<Term>),
+    App(Box<Term>, Box<Term>),
+}
