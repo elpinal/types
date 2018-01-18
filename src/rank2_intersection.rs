6b752a5d47df0e9b624b4bc92aa50e98e1c92a4a
elpinal <elpinal@users.noreply.github.com>
2018-01-18 17:17:14 +0900

    Define partial order for Rank1 and Rank2

--- src/rank2_intersection.rs
+++ src/rank2_intersection.rs
@@ -37,11 +37,70 @@ trait Types {
     fn ftv(&Self) -> HashSet<String>;
 }
 
+impl PartialOrd for Rank1 {
+    fn partial_cmp(&self, t: &Rank1) -> Option<Ordering> {
+        use std::cmp::Ordering::*;
+        let s1 = self.simple_types();
+        let s2 = t.simple_types();
+        if s1 == s2 {
+            Some(Equal)
+        } else if s1.is_subset(&s2) {
+            Some(Less)
+        } else if s2.is_superset(&s1) {
+            Some(Greater)
+        } else {
+            None
+        }
+    }
+}
+
+impl Rank1 {
+    fn simple_types(&self) -> HashSet<SimpleType> {
+        use self::Rank1::*;
+        let mut types = HashSet::new();
+        match *self {
+            Simple(ref s) => {
+                types.insert(s.clone());
+            }
+            Intersection(ref t1, ref t2) => {
+                types.extend(t1.simple_types());
+                types.extend(t2.simple_types());
+            }
+        }
+        types
+    }
+}
+
+impl PartialOrd for Rank2 {
+    fn partial_cmp(&self, t: &Rank2) -> Option<Ordering> {
+        use std::cmp::Ordering::*;
+        use self::Rank2::*;
+        match *self {
+            Simple(..) if self == t => Some(Equal),
+            Simple(..) => None,
+            Arr(ref t1, ref t2) => {
+                match *t {
+                    Simple(SimpleType::Var(..)) => None,
+                    Simple(SimpleType::Arr(ref s1, ref s2)) => {
+                        let o1 = Rank1::Simple(*s1.clone()).partial_cmp(t1);
+                        let o2 = t2.partial_cmp(&Box::new(Rank2::Simple(*s2.clone())));
+                        if o1 == o2 { o1 } else { None }
+                    }
+                    Arr(ref s1, ref s2) => {
+                        let o1 = s1.partial_cmp(t1);
+                        let o2 = t2.partial_cmp(s2);
+                        if o1 == o2 { o1 } else { None }
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test() {
-    }
+    fn test() {}
 }
