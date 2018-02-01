a415f85439e9abe170823acbd9d7388612f33e90
elpinal <elpinal@users.noreply.github.com>
2018-02-01 23:10:45 +0900

    Fix PartialOrd of Qual

--- src/linear.rs
+++ src/linear.rs
@@ -34,13 +34,18 @@ enum Pretype {
 impl PartialOrd for Qual {
     fn partial_cmp(&self, q: &Self) -> Option<Ordering> {
         use self::Qual::*;
+        use self::Ordering::*;
         if *self == Unrestricted && *q == Linear {
-            return Some(Ordering::Greater);
+            return Some(Greater);
         }
         if *self == Linear && *q == Unrestricted {
-            return Some(Ordering::Less);
+            return Some(Less);
         }
-        None
+        if self == q {
+            Some(Equal)
+        } else {
+            None
+        }
     }
 }
 
@@ -52,5 +57,25 @@ impl Qualified for Type {
     fn can_appear_in(&self, q1: Qual) -> bool {
         let Type(q2, _) = *self;
         q1 <= q2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_partial_ord_for_qual() {
+        use self::Qual::*;
+
+        assert!(Unrestricted <= Unrestricted);
+        assert!(Linear <= Unrestricted);
+        assert!(Linear <= Linear);
+
+        assert!(Unrestricted >= Unrestricted);
+        assert!(!(Linear >= Unrestricted));
+        assert!(Linear >= Linear);
+
+        assert!(!(Unrestricted <= Linear));
     }
 }
