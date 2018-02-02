6db62f1eb2cb47ef19cdf17ccfe6407817866b3f
elpinal <elpinal@users.noreply.github.com>
2018-02-02 23:17:50 +0900

    Add label_selective module

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub mod rank2_intersection;
 pub mod rank2_intersection2;
 pub mod rank2_system_f;
 pub mod linear;
+pub mod label_selective;
 
 pub trait Shift: Sized {
     fn shift_above(self, c: usize, d: isize) -> Self;
