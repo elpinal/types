3c25d95935a8b6fe97778847fa1af73328a046d3
elpinal <elpinal@users.noreply.github.com>
2018-02-04 23:19:59 +0900

    label_selective: make Term implement SubstRef and ShiftRef

--- src/label_selective.rs
+++ src/label_selective.rs
@@ -1,10 +1,36 @@
 //! Label-selective lambda calculus.
 
 pub mod symbolic {
+    use {Shift, ShiftRef, Subst, SubstRef};
+
     #[derive(Clone)]
     enum Term {
         Var(usize, usize),
         Abs(String, Box<Term>),
         App(String, Box<Term>, Box<Term>),
+    }
+
+    impl ShiftRef for Term {
+        fn shift_above_ref(&mut self, c: usize, d: isize) {
+            use self::Term::*;
+            let var = |x, n| Var(x as usize, n as usize);
+            let f = |c: usize, x: usize, n, t: &mut Term| if x >= c {
+                *t = var(x as isize + d, n as isize + d);
+            } else {
+                *t = Var(x, (n as isize + d) as usize);
+            };
+            self.map_ref(&f, c);
+        }
+    }
+
+    impl SubstRef for Term {
+        fn subst_ref(&mut self, j: usize, t: &Self) {
+            let f = |j, x, n, t0: &mut Self| if x == j {
+                *t0 = t.clone().shift(j as isize);
+            } else {
+                *t0 = Term::Var(x, n);
+            };
+            self.map_ref(&f, j)
+        }
     }
 }
