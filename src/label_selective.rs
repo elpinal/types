6acbfa7c62364bab7babf8ac86ce5122e2ce133f
elpinal <elpinal@users.noreply.github.com>
2018-02-04 23:19:19 +0900

    label_selective: add symbolic::Term

--- src/label_selective.rs
+++ src/label_selective.rs
@@ -1,1 +1,10 @@
+//! Label-selective lambda calculus.
 
+pub mod symbolic {
+    #[derive(Clone)]
+    enum Term {
+        Var(usize, usize),
+        Abs(String, Box<Term>),
+        App(String, Box<Term>, Box<Term>),
+    }
+}
